30c511edf168552e9e4cb1bdbd3d2d4762d7a30b
Patricio Pagano <pagano.patricio@gmail.com>
2022-03-06 00:25:52 -0300

    obra social

--- app.js
+++ app.js
@@ -9,7 +9,17 @@ let form = document.getElementById("form");
 // let Peso = document.getElementById("Peso");
 
 class Persona {
-  constructor(nombre, apellido, dni, altura, peso, anio, enfermedad, provincia) {
+  constructor(
+    nombre,
+    apellido,
+    dni,
+    altura,
+    peso,
+    anio,
+    enfermedad,
+    provincia,
+    obraSocial
+  ) {
     this.nombre = nombre;
     this.apellido = apellido;
     this.dni = dni;
@@ -19,6 +29,7 @@ class Persona {
     this.edad = new Date().getFullYear() - this.anio;
     this.enfermedad = enfermedad;
     this.provincia = provincia;
+    this.obraSocial = obraSocial;
   }
   IndiceMasaCorporal() {
     return (this.peso / this.altura ** 2).toFixed(2);
@@ -60,36 +71,44 @@ function showDatos() {
     form.peso.value,
     form.nacimiento.value,
     form.enfermedad.value,
-    form.provincia.value
+    form.provincia.value,
+    {}
   );
-  console.log(persona)
-  let container = document.getElementById("container");
-  if (persona.nombre !== "" && persona.apellido !== "" && persona.peso !== "" && persona.altura !== "" && persona.nacimiento !== 2022 && persona.provincia !== "-1") {Personas.push(persona);
-  localStorage.setItem("personas", JSON.stringify(Personas));
-  const ListaStorage = JSON.parse(localStorage.getItem("personas"));
-  if (ListaStorage !== null) {
-    
-    container.innerHTML = "";
-    let contenedor = document.createElement("div");
-    contenedor.innerHTML = `<div>
+  console.log(persona);
+  // let container = document.getElementById("container");
+  if (
+    persona.nombre !== "" &&
+    persona.apellido !== "" &&
+    persona.peso !== "" &&
+    persona.altura !== "" &&
+    persona.nacimiento !== 2022 &&
+    persona.provincia !== "-1"
+  ) {
+    Personas.push(persona);
+    localStorage.setItem("personas", JSON.stringify(Personas));
+    const ListaStorage = JSON.parse(localStorage.getItem("personas"));
+    if (ListaStorage !== null) {
+      container.innerHTML = "";
+      let contenedor = document.createElement("div");
+      contenedor.innerHTML = `<div>
                                       <h5>${persona.InfoDatos()}</h5>
                                       <h5>${persona.InfoPersona()}</h5>
                                       <h5>${persona.InfoProvincia()}</h5>
                                       <h5> Tiene un índice de masa corporal de ${persona.IndiceMasaCorporal()}</h5>
                                   </div>`;
 
-    container.appendChild(contenedor);
-    form.reset();
-  } else {
-    container.innerHTML = "";
-  }
-} else  {
-  container.innerHTML = `<div>
-  <h4> Es Obligatorio ingresar tus datos </h4>`;
-} 
+      container.appendChild(contenedor);
+      $("#obrasocial").css("display", "block");
 
+      form.reset();
+    } else {
+      container.innerHTML = "";
+    }
+  } else {
+    container.innerHTML = `<div>
+  <h4> Es Obligatorio ingresar tus datos </h4>`;
+  }
 }
-  
 
 //eventos botones
 let btnenviar = document.getElementById("btnenviar");
@@ -102,93 +121,102 @@ btnlimpiar.addEventListener("click", () => {
   clear();
 });
 
-
-
 // Datos obra social
-const obrasocial = [];
+const array_obrasocial = [];
 
 //clase ObraSocial
 
-class prepaga{
-    constructor(Nombre, precio){
-        this.id = obrasocial.length;
-        this.Nombre = Nombre.toUpperCase();
-        this.precio = Number(precio);
-    }
-    detalle(){
-        return `la obra social es ${this.Nombre} y su precio mensual es $ ${this.precio}`
-    }
+class prepaga {
+  constructor(Nombre, precio) {
+    this.id = obrasocial.length;
+    this.Nombre = Nombre.toUpperCase();
+    this.precio = Number(precio);
+  }
+  detalle() {
+    return `la obra social es ${this.Nombre} y su precio mensual es $ ${this.precio}`;
+  }
 }
 
 //enviar info al array
 
-obrasocial.push(new prepaga("Osbe",5000))
-obrasocial.push(new prepaga("Ominti",10000))
-obrasocial.push(new prepaga("Medicos",15000))
+array_obrasocial.push(new prepaga("Osbe", 5000));
+array_obrasocial.push(new prepaga("Ominti", 10000));
+array_obrasocial.push(new prepaga("Medicos", 15000));
 
-console.log(obrasocial);
-
+console.log(array_obrasocial);
 
 //funcion para generar el select de los obrasocial
 
-function obrasocialSelect(id){
-  let innerSelect = '';
-  obrasocial.forEach(OSocial => innerSelect += `<option value=${OSocial.id}>${OSocial.Nombre}</option>`)
+function obrasocialSelect(id) {
+  let innerSelect = "";
+  array_obrasocial.forEach(
+    (OSocial) =>
+      (innerSelect += `<option value=${OSocial.Nombre}>${OSocial.Nombre}</option>`)
+  );
   return `<select id="${id}">${innerSelect}</select>`;
 }
 
 //agregar al dom
-$("body").append(obrasocialSelect('elegirObraSocial'));
-$("#elegirObraSocial").change((e)=>{
-  const seleccion = obrasocial.find(obraso => obraso.id == e.target.value);
-  $("body").append(`<h4>${seleccion.detalle()}</h4>`)
-})
 
+$("#obrasocial").append(obrasocialSelect("elegirObraSocial"));
+$("#elegirObraSocial").change((e) => {
+  const seleccion = array_obrasocial.find(
+    (obraso) => obraso.id == e.target.value
+  );
+  console.log(seleccion);
+  $("#obrasocial").append(`<div id="ObraSocialSeleccionada">
+    <h4>Ud a seleccionado la obra social ${seleccion.Nombre} cuyo valor es de ${seleccion.precio}</h4>
+    <button type="button" id="confirmar">Confirmar</button>
+    <button type="button" id="cancelar">X</button>
+    </div>`);
+  //eventos botones
+  let seleccionObraSocial = document.getElementById("ObraSocialSeleccionada");
+  let btnConfirmar = document.getElementById("confirmar");
+  let btnCancelar = document.getElementById("cancelar");
 
+  //evento que escucha
+  btnConfirmar.addEventListener("click", () => {
+    alert(`A registrado su obra social correctamente`);
+    // persona.obraSocial = array_obrasocial.find(
+    //   (OS) => OS.Nombre === seleccion.Nombre
+    // );
+    seleccionObraSocial.innerHTML = "";
+  });
+  btnCancelar.addEventListener("click", () => {
+    seleccionObraSocial.innerHTML = "";
+  });
+});
 
 // AJAX
 
-
-function selectLista(array, id){
+function selectLista(array, id) {
   let innerSelect = "";
-  array.forEach(provincia => innerSelect += `<option value="${provincia.nombre}">${provincia.nombre}</option>`)
+  array.forEach(
+    (provincia) =>
+      (innerSelect += `<option value="${provincia.nombre}">${provincia.nombre}</option>`)
+  );
   return `<select id="${id}"> ${innerSelect}</select>`;
 }
 
-
 let URL = "https://apis.datos.gob.ar/georef/api/provincias";
-const provincias = [{id: -1, nombre: "seleccion de provincias"}];
+const provincias = [{ id: -1, nombre: "seleccion de provincias" }];
 
-
-$(document).ready(()=>{
+$(document).ready(() => {
   //llamado get de las provinciaas
-  $.get(URL,function (data, state) {
-          if(state === "success"){
-              provincias.push(...data.provincias);
-              console.log(provincias)
-              //agregamos el select
-              $("#containerselect").append(`<div>Seleccione su provincia: 
+  $.get(URL, function (data, state) {
+    if (state === "success") {
+      provincias.push(...data.provincias);
+      console.log(provincias);
+      //agregamos el select
+      $("#containerselect").append(`<div>Seleccione su provincia: 
               ${selectLista(provincias, "provincia")}</div>`);
-              $("#containerselect").append("<h4 id='salidaProvincias'></h4>");
-              $("#containerselect").change(function (e) { 
-                  
-                const seleccionados = provincias.find( obj => obj.id == e.target.value)
-                  //$("#salidaProvincias").html(`Ud ha seleccionado ${seleccionados.nombre}`)
-              });
-          }
+      $("#containerselect").append("<h4 id='salidaProvincias'></h4>");
+      $("#containerselect").change(function (e) {
+        const seleccionados = provincias.find(
+          (obj) => obj.id == e.target.value
+        );
+        //$("#salidaProvincias").html(`Ud ha seleccionado ${seleccionados.nombre}`)
       });
+    }
+  });
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
